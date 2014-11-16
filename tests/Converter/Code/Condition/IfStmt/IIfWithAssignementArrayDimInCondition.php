76f7f9c86c9fca3040cd4e2a288d7e2ff890c4e9
fezfez <demonchaux.stephane@gmail.com>
2014-11-16 15:23:24 +0000

    Add test case

---
+++ tests/Converter/Code/Condition/IfStmt/IIfWithAssignementArrayDimInCondition.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Code\Condition\IfStmt;
+
+class IfWithAssignementArrayDimInCondition
+{
+    public function test()
+    {
+        $toto = array(1 => true);
+
+		if ($averylongvariable = $toto[1]) {
+			echo 'tata';
+		}
+    }
+
+    public function testIncrementInArrayDim()
+    {
+        $i = 0;
+        $toto = array(1 => true);
+
+        if ($averylongvariable = $toto[$i++]) {
+            echo 'tata';
+        }
+    }
+}
