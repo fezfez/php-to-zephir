26396fe951cca44b0f1ff71033cd25f9461f823f
fezfez <demonchaux.stephane@gmail.com>
2014-11-16 17:58:10 +0100

    Apply sudo

--- bin/php-to-zephir.php
+++ bin/php-to-zephir.php
@@ -2,7 +2,6 @@
 use Symfony\Component\Console\Output\ConsoleOutput;
 use Symfony\Component\Console\Input\ArgvInput;
 use PhpToZephir\Service\CliFactory;
-
 chdir(realpath('./'));
 if (is_file(__DIR__ . '/../vendor/autoload.php') === true) {
     include_once __DIR__ . '/../vendor/autoload.php';
@@ -11,8 +10,6 @@ if (is_file(__DIR__ . '/../vendor/autoload.php') === true) {
 } else {
     throw new RuntimeException('Error: vendor/autoload.php could not be found. Did you run php composer.phar install?');
 }
-
-
 $output = new ConsoleOutput();
 $input  = new ArgvInput();
 $cli    = CliFactory::getInstance($output, $input);
