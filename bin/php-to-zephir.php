fe1834b6455df43620f7c527dddeeb9d078facd7
fezfez <demonchaux.stephane@gmail.com>
2015-02-07 12:57:51 +0100

    Test resolve lost commit

--- bin/php-to-zephir.php
+++ bin/php-to-zephir.php
@@ -3,6 +3,8 @@ use Symfony\Component\Console\Output\ConsoleOutput;
 use Symfony\Component\Console\Input\ArgvInput;
 use PhpToZephir\Service\CliFactory;
 
+ini_set('xdebug.max_nesting_level', 200);
+
 chdir(realpath('./'));
 
 if (is_file(__DIR__.'/../vendor/autoload.php') === true) {
