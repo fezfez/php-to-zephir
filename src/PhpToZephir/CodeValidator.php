838ff28549d5177cf48736843da488c8fe614cd5
fezfez <demonchaux.stephane@gmail.com>
2015-08-21 12:53:26 +0200

    Clean before validating code

--- src/PhpToZephir/CodeValidator.php
+++ src/PhpToZephir/CodeValidator.php
@@ -6,6 +6,7 @@ use Zephir\Commands\CommandGenerate;
 use Zephir\Config;
 use Zephir\Logger as ZephirLogger;
 use Zephir\CompilerException;
+use Zephir\Commands\CommandFullClean;
 
 class CodeValidator
 {
@@ -19,12 +20,15 @@ class CodeValidator
         if (!defined('ZEPHIRPATH'))
             define('ZEPHIRPATH', realpath(__DIR__.'/../../vendor/phalcon/zephir').'/');
 
+        
         $generateCommand = new CommandGenerate();
+        $cleanCommand = new CommandFullClean();
 
         try {
             $config = new Config();
             $config->set('namespace', $namespace);
             $config->set('silent', true);
+            $cleanCommand->execute($config, new ZephirLogger($config));
             $generateCommand->execute($config, new ZephirLogger($config));
         } catch (CompilerException $e) {
             throw new \Exception(sprintf('Error on %s', $e->getMessage()));
