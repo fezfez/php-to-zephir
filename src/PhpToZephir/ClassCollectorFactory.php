a2623e0f385e38a1b3fd70860efc9055e25a55c7
AzJezz <sgmati48@gmail.com>
2018-04-02 22:16:07 +0100

    Update ClassCollectorFactory.php

--- src/PhpToZephir/ClassCollectorFactory.php
+++ src/PhpToZephir/ClassCollectorFactory.php
@@ -5,11 +5,21 @@ namespace PhpToZephir;
 class ClassCollectorFactory
 {
     /**
+    * @var ClassCollector $collector;
+    */
+    private static $collector;
+    
+    private function __construct() {}
+    
+    /**
      * @return \PhpToZephir\ClassCollector
      */
     public static function getInstance()
     {
-        return new ClassCollector(
+        if(static::$collector !== null) {
+            return static::$collector;   
+        }
+        return static::$collector = new ClassCollector(
             new NodeFetcher(),
             new ReservedWordReplacer()
         );
